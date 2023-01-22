603bb4c699e4a1364be203b3db49981e39e65c18
Somanyu <somanyu.03samal@gmail.com>
2023-01-22 16:18:57 +0530

    🚑 fix: generate OTP in server

--- routes/contact.js
+++ routes/contact.js
@@ -8,9 +8,13 @@ router.get('/', (req, res) => {
 
 router.get('/info/:id', (req, res) => {
     const contactID = req.params.id;
+    
     // Find the user in the data store
     const contactDetails = contact.find(contact => contact.id === Number(contactID));
     if(contactDetails) {
+        
+        // Store contactDetails in locals in context to app
+        req.app.locals.contactDetails = contactDetails
         return res.status(200).json(contactDetails);
     } else {
         return res.status(404).json({message: "Contact not found"})
@@ -18,21 +22,28 @@ router.get('/info/:id', (req, res) => {
 
 })
 
+const generateRandom=()=>{
+    const random = Math.floor(100000 + Math.random() * 900000);
+    return random
+}
+
 router.post('/otp', async (req, res) => {
     const accountSID = process.env.TWILIO_ACCOUNT_SID
     const authToken = process.env.TWILIO_AUTH_TOKEN
     const client = require("twilio")(accountSID, authToken)
 
-    // const { text, phone } = req.body;
-    // console.log(req.body.text);
-    // return res.status(200).send("OK")
+    // Generate random 6-digit OTP. 
+    const OTP = generateRandom();
+
+    // Fetch details stored in contact.
+    const contact = res.app.locals.contactDetails;
 
     try {
         // Create Twilio message instance
         client.messages.create({
-            body: `Hola amigos!`,
+            body: `Hi ${contact.firstName}, your OTP is: ${OTP}`,
             from: '+14094032787',
-            to: `+916370112909`
+            to: `${contact.phone}`
         }).then(message => {
             console.log(`✅ Message sent. ${message.sid}`);
             return res.status(200).send({success: "Success"})
